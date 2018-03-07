d0169b880229b86c3c0050ff3d2c0c708bda4352
dexterchan <dexterchan@gmail.com>
2018-03-08 00:12:24 +0800

    currency checking

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var fs = require("fs");
 var zlib = require('zlib');
 var app = express();
 var fs = require("fs");
+const args = require("args-parser")(process.argv)
 
 const uuidv1 = require('uuid/v1');
 
@@ -11,26 +12,44 @@ var publicFolder=__dirname+'/public/';
 app.use(bodyParser.json({ type: 'application/json' }));
 app.use(express.static(publicFolder));
 
-var srcPath="/mnt/download/cryptoData/";
 
+var srcPath="/mnt/download/cryptoData/";
+if(args.p != undefined){
+    srcPath=args.p+"/";
+}
 
 app.get('/mkt/:ccy', function (req, res) {
-    var filename="live.mkt."+req.params.ccy+".csv";
-    var tempFile=publicFolder+filename+uuidv1()+".gz";
-    // Compress the file input.txt to input.txt.gz
-    fs.createReadStream(srcPath+filename)
-    .pipe(zlib.createGzip()).pipe(res);
-   // .pipe(fs.createWriteStream(tempFile))
+    var filename=srcPath+"live.mkt."+req.params.ccy+".csv";
+    fs.exists(filename, function(exists){
+        if (exists) {
+            var tempFile=publicFolder+filename+uuidv1()+".gz";
+            // Compress the file input.txt to input.txt.gz
+            fs.createReadStream(filename)
+            .pipe(zlib.createGzip()).pipe(res);
+        // .pipe(fs.createWriteStream(tempFile))
+        }else{
+            res.writeHead(400, {"Content-Type": "text/plain"});
+            res.end("ERROR mkt "+req.params.ccy+" does NOT Exists.");
+        }
+    });
 
  });
 
  app.get('/tweet/:ccy', function (req, res) {
-    var filename="live.tweet."+req.params.ccy+".csv";
-    var tempFile=publicFolder+filename+uuidv1()+".gz";
-    // Compress the file input.txt to input.txt.gz
-    fs.createReadStream(srcPath+filename)
-    .pipe(zlib.createGzip()).pipe(res);
-   // .pipe(fs.createWriteStream(tempFile))
+    var filename=srcPath+"live.tweet."+req.params.ccy+".csv";
+    fs.exists(filename, function(exists){
+        if (exists) {    
+            var tempFile=publicFolder+filename+uuidv1()+".gz";
+            // Compress the file input.txt to input.txt.gz
+            fs.createReadStream(filename)
+            .pipe(zlib.createGzip()).pipe(res);
+        // .pipe(fs.createWriteStream(tempFile))
+        }else{
+            res.writeHead(400, {"Content-Type": "text/plain"});
+            res.end("ERROR tweet "+req.params.ccy+" does NOT Exists.");
+        }
+
+    });
 
  });
 
